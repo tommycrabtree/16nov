375c6fc151de370944f304fb703388e0775cd3f4
Tommy <tommycrabtree@outlook.com>
2025-11-16 22:24:14 -0500

    End of Section 3

---
+++ client/src/app/app.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('client');
+  protected members = signal<any>([]);
+
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the request, playa')
+    })
+  }
+}
